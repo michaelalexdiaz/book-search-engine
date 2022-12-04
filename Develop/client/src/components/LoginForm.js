6f69f50f07cf0f2bb9c124356dcf14e9b1714e08
michaelalexdiaz <michaelalexdiaz@gmail.com>
2022-12-04 14:42:43 -0600

    Update LoginForm.js

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 // importing apollo and mutation
 import { useMutation } from '@apollo/react-hooks';
